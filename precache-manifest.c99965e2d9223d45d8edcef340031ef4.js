78d9c8fa3a54e0a64fa822ded66447ee8814aca3
harshitmodi <hmodi9365@gmail.com>
2019-05-21 15:02:19 -0500

    Updates

---
+++ precache-manifest.c99965e2d9223d45d8edcef340031ef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431b943cd83b1c01ada78c3c3cfac7b0",
+    "url": "/view/index.html"
+  },
+  {
+    "revision": "10baefa1534b392bb5d0",
+    "url": "/view/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0b8db30f5d687922b5d2",
+    "url": "/view/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "10baefa1534b392bb5d0",
+    "url": "/view/static/js/2.07de84a7.chunk.js"
+  },
+  {
+    "revision": "0b8db30f5d687922b5d2",
+    "url": "/view/static/js/main.fa0f2d2f.chunk.js"
+  },
+  {
+    "revision": "fed8fc1f28342a74693d",
+    "url": "/view/static/js/runtime~main.140575cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/view/static/media/logo.5d5d9eef.svg"
+  }
+]);
